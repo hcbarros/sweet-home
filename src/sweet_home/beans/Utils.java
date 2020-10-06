4ed878d88c4a58e47b178556f4096083b2c0488e
henriquecbarrosIFPE <72453785+henriquecbarrosIFPE@users.noreply.github.com>
2020-10-06 12:20:17 -0300

    commit

---
+++ src/sweet_home/beans/Utils.java
@@ -0,0 +1,114 @@
+package sweet_home.beans;
+
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+import sweet_home.Imovel;
+import sweet_home.Telefone;
+import sweet_home.Usuario;
+import sweet_home.servico.ImovelServico;
+import sweet_home.servico.TelefoneServico;
+import sweet_home.servico.UsuarioServico;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Default;
+import javax.faces.annotation.FacesConfig;
+import javax.faces.annotation.FacesConfig.Version;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+
+import org.apache.commons.io.IOUtils;
+
+
+@ManagedBean
+@RequestScoped
+public class Utils implements Serializable {
+
+private static final long serialVersionUID = 1L;
+	
+	@EJB
+    private ImovelServico imovelServico; 
+	@EJB
+    private TelefoneServico telefoneServico; 
+	@EJB
+    private UsuarioServico usuarioServico; 
+	
+    private static List<Imovel> listaImoveis = null;
+    private static List<Telefone> listaTelefones = null;
+    private static Usuario usuario = null;
+            
+    
+    @PostConstruct
+    public void init() {    	            
+    	
+       	HttpSession sessao = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(false);
+    	usuario = (Usuario) sessao.getAttribute("logado");
+    }
+        
+   
+    
+    public List<Telefone> telefones(String email) {
+    	
+    	return getListaTelefones().stream().filter(x -> x.getUsuario().getEmail().equals(email))
+    			.collect(Collectors.toList());
+    }
+    
+    
+    public String direcionarMeusImoveis() {
+   	
+    	return "meusImoveis";
+    }
+    
+    public String direcionarTodosImoveis() {
+    	
+    	return "todosImoveis";
+    }   
+    
+    public String voltar() {
+    	
+    	return "voltar";
+    }
+    
+    
+    public List<Imovel> meusimoveis() {
+             	    	   	
+    	return getListaImoveis().stream()
+    			.filter(x -> x.getUsuario().getEmail().equals(usuario.getEmail()))
+    			.collect(Collectors.toList());    	    	
+    }
+     
+    
+    public List<Telefone> getListaTelefones() {
+    	listaTelefones = telefoneServico.consultarTelefones();
+        return listaTelefones;
+    }
+        
+    public void setListaTelefones() {
+    	listaTelefones = telefoneServico.consultarTelefones();
+    }
+    
+    public List<Imovel> getListaImoveis() {
+    	listaImoveis = imovelServico.recuperarImoveis();
+        return listaImoveis;
+    }
+        
+    public void setListaImoveis() {
+    	listaImoveis = imovelServico.recuperarImoveis();
+    }
+    
+       
+}
